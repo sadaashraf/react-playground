8124d0ac415889635494be9194f922b061a4653d
Ashraf <nayeemashraf92@gmail.com>
2026-06-04 10:34:36 +0500

    add joke

--- learning pro/backend/index.js
+++ learning pro/backend/index.js
@@ -1,18 +1,32 @@
-require('dotenv').config();
-const express = require('express');
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/login', (req, res) => {
-  res.send('Login Page');
-});
-
-app.get('/register', (req, res) => {
-  res.send('Register Page');
+app.get('/jokes', (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: 'A joke',
+      content: 'Why don’t scientists trust atoms? Because they make up everything.',
+    },
+    {
+      id: 2,
+      title: 'Another joke',
+      content: 'Why did the scarecrow win an award? Because he was outstanding in his field.',
+    },
+    {
+      id: 3,
+      title: 'Yet another joke',
+      content: 'Why don’t skeletons fight each other? They don’t have the guts.',
+    }
+  ];
+  res.json(jokes);
 });
 
 app.listen(process.env.PORT, () => {
--- learning pro/src/App.jsx
+++ learning pro/src/App.jsx
@@ -3,19 +3,37 @@ import Form from "./component/form"
 import EmployeeCard from "./component/prop"
 import User from "./component/state"
 import Users from "./component/useEffect"
-
+import axios from "axios"
+import { useState, useEffect } from "react"
 function App() {
-
+  const [jokes, setJokes] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:4000/jokes')
+      .then(response => {
+        setJokes(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching jokes:', error);
+      });
+  }, []);
   return (
     <div>
-      <Form />
+      <p>{jokes.length} jokes</p>
+      {jokes.map(joke => (
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      ))}
+
+      {/* <Form />
       <Users />
       <User />
       <Event />
       <h1>Employee List</h1>
       <EmployeeCard name="John Doe"
         position="Software Engineer"
-        salary={80000} />
+        salary={80000} /> */}
     </div>
   )
 }
